4b2a2266c8676bf9ee8128ff097be37537b741dd
PhaniNVN <n.v.nphanindra@gmail.com>
2026-03-22 13:51:58 +0530

    Update duckdb_quries.sql

--- part5-datalake/duckdb_quries.sql
+++ part5-datalake/duckdb_quries.sql
@@ -1,1 +1,48 @@
 
+-- Q1: List all customers along with the total number of orders they have placed
+SELECT 
+    c.customer_id,
+    c.customer_name,
+    COUNT(o.order_id) AS total_orders
+FROM read_csv_auto('customers.csv') c
+LEFT JOIN read_json_auto('orders.json') o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name;
+
+-- Q2: Find the top 3 customers by total order value
+
+SELECT 
+    c.customer_id,
+    c.customer_name,
+    SUM(o.total_amount) AS total_spent
+FROM read_csv_auto('customers.csv') c
+JOIN read_json_auto('orders.json') o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name
+ORDER BY total_spent DESC
+LIMIT 3;
+
+-- Q3: List all products purchased by customers from Bangalore
+
+SELECT DISTINCT 
+    p.product_name
+FROM read_csv_auto('customers.csv') c
+JOIN read_json_auto('orders.json') o
+ON c.customer_id = o.customer_id
+JOIN read_parquet('products.parquet') p
+ON o.order_id = p.order_id
+WHERE c.city = 'Bangalore';
+
+
+-- Q4: Join all three files to show customer name, order date, product name, and quantity
+
+SELECT 
+    c.customer_name,
+    o.order_date,
+    p.product_name,
+    p.quantity
+FROM read_csv_auto('customers.csv') c
+JOIN read_json_auto('orders.json') o
+ON c.customer_id = o.customer_id
+JOIN read_parquet('products.parquet') p
+ON o.order_id = p.order_id;
